2c03a13607c0e1620e149f391d6699d6cc7764c8
Matteo <matteo.calvanico@studio.unibo.it>
2024-03-23 18:57:00 +0100

    Finish Deck class

--- src/main/java/it/unibo/virtualCasino/model/Deck.java
+++ src/main/java/it/unibo/virtualCasino/model/Deck.java
@@ -1,21 +1,27 @@
 package it.unibo.virtualCasino.model;
 
+import it.unibo.virtualCasino.model.utils.CardSeed;
+import it.unibo.virtualCasino.model.utils.CardColor;
+import it.unibo.virtualCasino.model.utils.CardNumber;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Deck {
     private List<Card> deck;
 
-    public Deck(){
-        this.deck = createDeck();
-    }
-
     /**
+     * Every deck as 13 card for each seed. Example: "A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠"
      * @return a brand new deck with 52 card
      */
-    private List<Card> createDeck(){
-        // TODO finish this
-        throw new UnsupportedOperationException("Unimplemented method 'createDeck'");
+    public Deck(){
+        this.deck = new ArrayList<>();
+        for (CardSeed s : CardSeed.values()) { //We iterate for each seed
+            for (CardNumber c : CardNumber.values()) { //For each seed we associate a number
+                Card newC = new Card(c, s, (s == CardSeed.HEARTS || s == CardSeed.DIAMONDS) ? CardColor.RED : CardColor.BLACK); //The Heart and Diamonds have red cards, black for the other
+                this.deck.add(newC);
+            }
+        }
     }
 
     /**
@@ -31,5 +37,17 @@ public class Deck {
     */
     public Card draw(){
         return (deck.isEmpty()) ? null : deck.remove(0);
+    }
+
+    /**
+     * @return the info of the deck
+    */
+    @Override
+    public String toString() {
+        String info = "Deck: ";
+        for (Card card : deck) {
+            info += card.toString();
+        }
+        return info;
     }
 }
