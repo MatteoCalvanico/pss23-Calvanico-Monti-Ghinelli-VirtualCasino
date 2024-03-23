c7d4f3bec09d895e27c868cbba76ba19adb3bfa2
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-03-23 19:09:17 +0100

    Refactor code

---
+++ src/main/java/it/unibo/virtualCasino/model/Games/Impl/Utils/RouletteBetTypes.java
@@ -0,0 +1,15 @@
+package it.unibo.virtualCasino.model.Games.Impl.Utils;
+
+public enum RouletteBetTypes {
+  STRAIGHT_UP,
+  SPLIT,
+  STREET,
+  CORNER,
+  TOP_LINE,
+  DOUBLE_STREET,
+  COLUMN,
+  DOZEN,
+  ODD_EVEN,
+  RED_BLACK,
+  HALF
+}
