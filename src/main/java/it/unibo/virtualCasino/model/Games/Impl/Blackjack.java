fb9eca3fd5e82114c84236d29aad49bf56e27653
Matteo <matteo.calvanico@studio.unibo.it>
2024-03-23 20:13:32 +0100

    Create initial structure of Blackjack class

--- src/main/java/it/unibo/virtualCasino/model/Games/Impl/Blackjack.java
+++ src/main/java/it/unibo/virtualCasino/model/Games/Impl/Blackjack.java
@@ -1,8 +1,37 @@
 package it.unibo.virtualCasino.model.Games.Impl;
 
 import it.unibo.virtualCasino.model.Games.Games;
+import it.unibo.virtualCasino.model.Deck;
+import it.unibo.virtualCasino.model.Card;
+import java.util.List;
 
 public class Blackjack implements Games{
+
+    /**
+     * In blackjack we use 2 to 6 normal deck [52 card each]
+     */
+    private List<Deck> playDeck;
+
+    /**
+     * The cards the player have. The player have 2 deck, the normal one and one if he want to split
+     */
+    private Deck[] playerDeck;
+
+    /**
+     * The cards in the bench
+     */
+    private Deck dealerDeck;
+
+    /**
+     * Same thing with the playerDeck, in the 0 position we have the current bet and in the 1 position the bet for the split
+     */
+    private double[] bet;
+
+    /**
+     * The player can decide to make an insurance if the dealer have an ACE. 
+     * If the dealer do a blackjack and the insurance is true the player don't lose any money
+     */
+    private boolean insurance; 
 
     @Override
     public void bet(double amount) {
