f2bfd7e7a96a02e5543cb9768d2f9ee95ea22b2d
Matteo <matteo.calvanico@studio.unibo.it>
2024-03-23 18:55:29 +0100

    Modify enum CardNumber

--- src/main/java/it/unibo/virtualCasino/model/utils/CardNumber.java
+++ src/main/java/it/unibo/virtualCasino/model/utils/CardNumber.java
@@ -4,32 +4,40 @@ package it.unibo.virtualCasino.model.utils;
  * Enums representing the number of French cards
 */
 public enum CardNumber {
-    ACE_ONE(1),
-    ACE_ELEVEN(11), //The ACE can be both one or eleven [PLAYER DECIDE]
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+  ACE_ONE(1, "ACE"),
+  ACE_ELEVEN(11, "ACE"), // The ACE can be both one or eleven [PLAYER DECIDE]
+  TWO(2, "TWO"),
+  THREE(3, "THREE"),
+  FOUR(4, "FOUR"),
+  FIVE(5, "FIVE"),
+  SIX(6, "SIX"),
+  SEVEN(7, "SEVEN"),
+  EIGHT(8, "EIGHT"),
+  NINE(9, "NINE"),
+  TEN(10, "TEN"),
+  JACK(10, "JACK"),
+  QUEEN(10, "QUEEN"),
+  KING(10, "KING");
+
+  private final int value;
+  private final String displayName;
   
-    private final int value;
-  
-    private CardNumber(int val) { //The constructor is private because the cards' value are already set
-      this.value = val;
-    }
-  
-    /**
-     * @return the value of the card
-     */
-    public int getValue() {
+  private CardNumber(int val, String displayName) { //The constructor is private because the cards' value are already set
+    this.value = val;
+    this.displayName = displayName;
+  }
+
+  /**
+   * @return the value of the card
+   */
+  public int getValue() {
       return this.value;
-    }
-  
+  }
+
+  /**
+   * @return the displayName of the card
+   */
+  public String getName() {
+      return this.displayName;
+  }
 }
