239245f59484c833b6b8681f610f0b3427b042fe
Matteo <matteo.calvanico@studio.unibo.it>
2024-03-24 14:02:54 +0100

    Add few utility method in deck

--- src/main/java/it/unibo/virtualCasino/model/Deck.java
+++ src/main/java/it/unibo/virtualCasino/model/Deck.java
@@ -11,17 +11,39 @@ public class Deck {
     private List<Card> deck;
 
     /**
-     * Every deck as 13 card for each seed. Example: "A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠"
-     * @return a brand new deck with 52 card
+     * Init a empty deck
      */
     public Deck(){
         this.deck = new ArrayList<>();
+    }
+
+    /**
+     * Popolate the deck with 52 card. 
+     * Every deck as 13 card for each seed. Example: "A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠"
+     */
+    public void initPlayDeck(){
         for (CardSeed s : CardSeed.values()) { //We iterate for each seed
             for (CardNumber c : CardNumber.values()) { //For each seed we associate a number
                 Card newC = new Card(c, s, (s == CardSeed.HEARTS || s == CardSeed.DIAMONDS) ? CardColor.RED : CardColor.BLACK); //The Heart and Diamonds have red cards, black for the other
                 this.deck.add(newC);
             }
         }
+    }
+
+    /**
+     * Look at a specific card in the deck
+     * @param pos where to get the card
+     * @return the card from the position (doesn't remove)
+     */
+    public Card checkCardFromDeck(int pos){
+        return this.deck.get(pos);
+    }
+
+    /**
+     * @return the number of card in the deck
+     */
+    public int size(){
+        return this.deck.size();
     }
 
     /**
@@ -37,6 +59,21 @@ public class Deck {
     */
     public Card draw(){
         return (deck.isEmpty()) ? null : deck.remove(0);
+    }
+
+    /**
+     * Remove all the card from the deck
+     */
+    public void emptyDeck(){
+        this.deck.clear();
+    }
+
+    /**
+     * Insert a new card in the deck
+     * @param c the card to insert
+     */
+    public void insert(Card c){
+        this.deck.add(c);
     }
 
     /**
