5fa77a20196577b8be4ff67e7b05da718e77b313
Matteo <matteo.calvanico@studio.unibo.it>
2024-03-24 14:34:03 +0100

    Modify Blackjack

--- src/main/java/it/unibo/virtualCasino/model/Games/Impl/Blackjack.java
+++ src/main/java/it/unibo/virtualCasino/model/Games/Impl/Blackjack.java
@@ -4,6 +4,7 @@ import it.unibo.virtualCasino.model.Games.Games;
 import it.unibo.virtualCasino.model.Deck;
 import it.unibo.virtualCasino.model.Card;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Blackjack implements Games{
 
@@ -33,6 +34,27 @@ public class Blackjack implements Games{
      */
     private boolean insurance; 
 
+    /**
+     * Init all we need for play Blackjack
+     * @param numberOfDeck how many deck of card we want to use
+     */
+    public Blackjack(int numberOfDeck){
+        this.playDeck = new ArrayList<>();
+        for(int i = 0; i < numberOfDeck; i++){ //Creation and adding of the number of playing deck
+            Deck newDeck = new Deck();
+            newDeck.initPlayDeck();
+            this.playDeck.add(newDeck);
+        }
+        this.playerDeck = new Deck[2];
+        this.playerDeck[0] = new Deck();
+        this.playerDeck[1] = new Deck();
+
+        this.dealerDeck = new Deck();
+
+        this.bet = new double[2];
+        this.insurance = false;
+    }
+
     @Override
     public void bet(double amount) {
         // TODO Auto-generated method stub
@@ -50,5 +72,38 @@ public class Blackjack implements Games{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'showResult'");
     }
-    
+
+    /**
+     * Ask the dealer for another card
+     */
+    public void call(){
+        // TODO
+    }
+
+    /**
+     * Check if the combination of card is a blackjack
+     */
+    private boolean isBlackjack(){
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'isBlackjack'");
+    }
+
+    /**
+     * Check if the split is possible
+     * @return true if possible, false otherwise
+     */
+    private boolean isSplit(){
+        return (this.playerDeck[0].checkCardFromDeck(0).getCardNumber() == this.playerDeck[0].checkCardFromDeck(1).getCardNumber()) ? true : false;
+    }
+
+    /**
+     * Take one card from the main player deck and put in the second deck [ONLY IF THE DECK HAVE TWO CARD WITH THE SAME VALUE]
+     */
+    public void split(){
+        if (isSplit()) {
+            this.playerDeck[1].insert(this.playerDeck[0].draw());
+        }else{
+            throw new UnsupportedOperationException("Impossible to split"); // TODO
+        }
+    }
 }
