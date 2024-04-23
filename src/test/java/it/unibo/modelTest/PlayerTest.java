dcd778b54c082c573fa49e2ce91919334e09a146
Filippo Monti <filippomonti2003@gmail.com>
2024-04-23 12:44:22 +0200

    finish player test

--- src/test/java/it/unibo/modelTest/PlayerTest.java
+++ src/test/java/it/unibo/modelTest/PlayerTest.java
@@ -2,11 +2,30 @@ package it.unibo.modelTest;
 
 import it.unibo.virtualCasino.model.Player;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest{
-    Player p = new Player("pippo");
-    @Test void testInitBalance(){
+    Player p;
+
+    @BeforeEach
+    void setup(){
+        System.out.println("!!! START Player TEST !!!");
+        p = new Player("TESTPLAYER");
+    }
+
+    @Test void testInitBalance(){ //Test if the initial balance is 1000
         assertEquals(1000, p.getAccount());
-    } 
+    }
+    
+    @Test void testAddWin(){//Test the correct add amount of credit
+        p.addWin(500);
+        assertEquals(1500, p.getAccount());
+    }
+
+    @Test void testRemoveLoss(){//Test the correct remove the amount of credit
+        p.removeLoss(1000);
+        assertEquals(0, p.getAccount());
+    }
 }
