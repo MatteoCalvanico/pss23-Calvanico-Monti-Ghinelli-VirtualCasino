4bbb17e30ab295fb03b2dad0bfbb131718be8070
Matteo <matteo.calvanico@studio.unibo.it>
2024-04-27 19:59:52 +0200

    Add blackjack FXML and the view

--- src/main/java/it/unibo/virtualCasino/App.java
+++ src/main/java/it/unibo/virtualCasino/App.java
@@ -4,6 +4,7 @@
 package it.unibo.virtualCasino;
 
 
+import it.unibo.virtualCasino.view.blackjack.blackjackView;
 import javafx.application.Application;
 
 /** Main application entry-point's class. */
@@ -16,7 +17,7 @@ public final class App {
      * @param args
      */
     public static void main(final String[] args) {
-        Application.launch(JavaFXApp.class, args);
+        Application.launch(blackjackView.class, args);
         // The following line raises: Error: class it.unibo.samplejavafx.App is not a subclass of javafx.application.Application
         // JavaFXApp.launch(args);
         // While the following would do just fine:
---
+++ src/main/java/it/unibo/virtualCasino/view/blackjack/blackjackView.java
@@ -0,0 +1,24 @@
+package it.unibo.virtualCasino.view.blackjack;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public final class blackjackView extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/blackjack.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Remember...the house always win.");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
