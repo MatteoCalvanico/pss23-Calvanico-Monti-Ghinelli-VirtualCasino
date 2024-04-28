ce887bf6ee38ac9f055069fa16e91bf74a1e6f52
Filippo Monti <filippomonti2003@gmail.com>
2024-04-28 12:24:27 +0200

    created complete test for 'initPlayDeck' and for 'check Card From Deck'

--- src/test/java/it/unibo/modelTest/DeckTest.java
+++ src/test/java/it/unibo/modelTest/DeckTest.java
@@ -1,9 +1,8 @@
 package it.unibo.modelTest;
-import it.unibo.virtualCasino.model.games.impl.blackjack.Card;
+
 import it.unibo.virtualCasino.model.games.impl.blackjack.Deck;
 import it.unibo.virtualCasino.model.games.impl.blackjack.utils.CardColor;
-import it.unibo.virtualCasino.model.games.impl.blackjack.utils.CardNumber;
-import it.unibo.virtualCasino.model.games.impl.blackjack.utils.CardSeed;
+import it.unibo.virtualCasino.model.games.impl.blackjack.Card;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,14 +20,39 @@ public class DeckTest {
     //testo il numero di carte
     @Test void testInitPlayDeck(){
         d.initPlayDeck();
-        assertEquals(56, d.size()); //there are 56 cards because the ace and the one are two distinct cards 
+        assertEquals(56, d.size()); //there are 56 cards because the ace and the one are two distinct cards
     }
 
-    @Test
-    public void testInitPlayDeck_shouldHaveAllRanksForEverySeed() {
+    @Test void testInitPlayDeckColorSeed(){
         d.initPlayDeck();
+        //inizializzo due contatori a zero
+        int redCardCount = 0;
+        int blackCardCount = 0;
 
-        int expectedCards = CardSeed.values().length * CardNumber.values().length;
-        assertEquals(expectedCards, d.size());//Calcolo il numero atteso di carte moltiplicando il numero di semi per il numero di valori delle carte e poi lo confrontiamo con la dimensione effettiva
+        for (int i = 0; i < d.size(); i++){//uso d.size per ottenere la dimensione del mazzo per poi iterare usando un indice
+            Card card = d.checkCardFromDeck(i);
+            if (card.getCardColor() == CardColor.RED) {
+                redCardCount++;
+            }else if (card.getCardColor() == CardColor.BLACK){
+                blackCardCount++;
+            }else{
+               fail("Unexpected card color found: " + card.getCardColor()); //Se viene trovato un colore diverso da rosso o nero il test fallisce
+            }
+        }
+        assertEquals(28, redCardCount);
+        assertEquals(28, blackCardCount);
+        assertEquals(56, d.size());
+
     }
+
+    @Test public void testCheckCardFromDeck() {
+        d.initPlayDeck();
+        Card firstCard = d.checkCardFromDeck(51);
+        assertNotNull(firstCard, "I check that the first card is nothing");
+        // Check if the retrieved card remains in the deck (not removed by checkCardFromDeck)
+        assertEquals(56, d.size());
+    }
+
+
+
 }
