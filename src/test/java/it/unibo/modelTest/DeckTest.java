f83aa164d18d5abdddefc0fb644794b00b49c277
Filippo Monti <filippomonti2003@gmail.com>
2024-05-01 11:24:13 +0200

    finished testing the countCard() method

--- src/test/java/it/unibo/modelTest/DeckTest.java
+++ src/test/java/it/unibo/modelTest/DeckTest.java
@@ -2,6 +2,8 @@ package it.unibo.modelTest;
 
 import it.unibo.virtualCasino.model.games.impl.blackjack.Deck;
 import it.unibo.virtualCasino.model.games.impl.blackjack.utils.CardColor;
+import it.unibo.virtualCasino.model.games.impl.blackjack.utils.CardNumber;
+import it.unibo.virtualCasino.model.games.impl.blackjack.utils.CardSeed;
 import it.unibo.virtualCasino.model.games.impl.blackjack.Card;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +25,7 @@ public class DeckTest {
         assertEquals(56, d.size()); //there are 56 cards because the ace and the one are two distinct cards
     }
 
+    //testo se ci sono 28 carte nere e 28 carte rosse
     @Test void testInitPlayDeckColorSeed(){
         d.initPlayDeck();
         //inizializzo due contatori a zero
@@ -41,11 +44,11 @@ public class DeckTest {
         }
         assertEquals(28, redCardCount);
         assertEquals(28, blackCardCount);
-        assertEquals(56, d.size());
-
+        assertEquals(56, d.size()); //I check that the deck has 56 cards
     }
 
-    @Test public void testCheckCardFromDeck() {
+    //testo se prendendo una carta ritorna poi nella sua posizione
+    @Test public void testCheckCardFromDeck(){
         d.initPlayDeck();
         Card firstCard = d.checkCardFromDeck(51);
         assertNotNull(firstCard, "I check that the first card is nothing");
@@ -53,6 +56,23 @@ public class DeckTest {
         assertEquals(56, d.size());
     }
 
+    @Test public void testCountCardes(){
+        //faccio un mazzo vuoto e aggiungo 2 carte
+        Deck de = new Deck();
+        Card card1 = new Card(CardNumber.TWO, CardSeed.SPADES, CardColor.BLACK);
+        Card card2 = new Card(CardNumber.KING, CardSeed.HEARTS, CardColor.RED);
+
+        //utilizzo il metodo insert() per aggiungere le due carte  
+        de.insert(card1);
+        de.insert(card2);
+        
+        //calcolo somma prevista dei valori delle carte 
+        int expectedSum = CardNumber.TWO.getValue() + CardNumber.KING.getValue();
+
+        //chiamo metodo countCard() e verifico del risultato
+        int actualSum = de.countCard();
+        assertEquals(expectedSum, actualSum);//expectedSum=12    
+    }
 
 
 }
