426ee6ecfcafaf4bada220ea8c9dd3fa78399956
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-05-10 16:25:09 +0200

    added roulette view info class

---
+++ src/main/java/it/unibo/virtualCasino/view/roulette/utils/RouletteViewInfo.java
@@ -0,0 +1,8 @@
+package it.unibo.virtualCasino.view.roulette.utils;
+
+/**
+ * Static class containing magin numbers for roulette view ui components
+ */
+public class RouletteViewInfo {
+  public static double LINE_END_X = 98;   
+}
