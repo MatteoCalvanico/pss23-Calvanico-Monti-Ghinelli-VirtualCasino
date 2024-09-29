40dec026e060e2eccbe46ff025462e7ed5dec5e6
Matteo <matteo.calvanico@studio.unibo.it>
2024-09-29 16:54:32 +0200

    Added bet function

--- src/main/java/it/unibo/virtualCasino/controller/blackjack/BlackjackController.java
+++ src/main/java/it/unibo/virtualCasino/controller/blackjack/BlackjackController.java
@@ -1,7 +1,6 @@
 package it.unibo.virtualCasino.controller.blackjack;
 
 import it.unibo.virtualCasino.controller.BaseController;
-import it.unibo.virtualCasino.model.Player;
 import it.unibo.virtualCasino.model.games.impl.blackjack.Blackjack;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +8,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class BlackjackController extends BaseController {
     @FXML
@@ -77,6 +78,14 @@ public class BlackjackController extends BaseController {
     @FXML 
     private Button btnBetMinus100;
 
+    @FXML
+    private Button btnSetBet;
+
+
+    private Blackjack BJGame;
+
+    private int currentBet = 0;
+
 
     /**Per creare un metodo e linkarlo fare:
      * 1) Creare nel tag all'interno del fxml il giusto evento, es: <Button text='Ciao' onAction='#method'/>
@@ -85,7 +94,7 @@ public class BlackjackController extends BaseController {
 
     @Override
     protected void setGame() {
-        Blackjack BJGame = new Blackjack(6, currentPlayer);
+        BJGame = new Blackjack(6, currentPlayer);
 
         // Set the player name and balance
         txtPlayer.setText(currentPlayer.getName());
@@ -100,5 +109,48 @@ public class BlackjackController extends BaseController {
         Image chip = new Image(getClass().getResourceAsStream("/sprite/chips/chipBlackWhite.png"));
         ImageView chipView = new ImageView(chip); // We gonna put this in the HBox
         chipBox.getChildren().add(chipView);
+    }
+
+    @FXML
+    /*
+    * Method to add 100 to the current bet
+    */
+    protected void addBet(){
+        currentBet += 100;
+        txtBet0.setText(Integer.toString(currentBet));
+    }
+
+    @FXML
+    /*
+    * Method to remove 100 to the current bet
+    */
+    protected void removeBet(){
+        if(currentBet >= 100){
+            currentBet -= 100;
+            txtBet0.setText(Integer.toString(currentBet));
+        }
+    }
+
+    @FXML
+    /*
+    * Method to set the ultimate bet
+    * 
+    * !!! The balance don't change until the game is over !!!
+    */
+    protected void setBet(){
+        try {
+            BJGame.bet(currentBet);
+
+            // Disable the buttons to change the bet
+            btnBet100.disableProperty().set(true); 
+            btnBetMinus100.disableProperty().set(true);
+            btnSetBet.disableProperty().set(true);
+        } catch (Exception e) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Bet Warning");
+            alert.setHeaderText(null);
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
     }
 }
