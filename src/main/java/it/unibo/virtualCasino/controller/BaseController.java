83863f84e7440d6f7342c34a0976d73bf50f4c59
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-10-21 22:26:03 +0200

    Makes base controller more generic

--- src/main/java/it/unibo/virtualCasino/controller/BaseController.java
+++ src/main/java/it/unibo/virtualCasino/controller/BaseController.java
@@ -1,74 +1,130 @@
 package it.unibo.virtualCasino.controller;
 
-// Import for FXML
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-// Import for sound
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import it.unibo.virtualCasino.controller.singleton.PlayerHolder;
 import it.unibo.virtualCasino.model.Player;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
- * Base class that define the base methods of a controller:
- * <pre>
-- initialize: when the FXML file is launch call receiveData() and setGame();
-- sendData: invoked when you need to change scene, save the player in the singleton class;
-- receiveData: method to take the instance of the player thanks to the singleton class
-- setGame: needs to be override to set your own game.
-* </pre>
-*/
+ * Base class that define the base methods of a controller
+ */
 public abstract class BaseController implements Initializable {
 
-    protected Player currentPlayer; 
+    protected Player currentPlayer;
 
     /**
-     * Do something when the FXML file is loaded
+     * Initializes the controller when the FXML file is loaded
      */
     @FXML
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         receiveData();
-        setGame();
+        setBaseController();
     }
 
     /**
-     * Method to be invoked when you need to change scene, save the player in the singleton class
+     * Override this method to set the base contoller
+     */
+    abstract protected void setBaseController();
+
+    /**
+     * Method to be invoked when you need to change scene, save the player in the
+     * singleton class
      */
     @FXML
-    protected void sendData(){
+    protected void sendData() {
         // Save the player in the singleton class
         PlayerHolder holderInstance = PlayerHolder.getInstance();
         holderInstance.setPlayerHolded(this.currentPlayer);
     }
 
-    //Method to take the instance of the player thanks to the singleton class
+    /**
+     * Method to take the instance of the player thanks to the singleton class
+     */
     @FXML
     private void receiveData() {
         PlayerHolder holderInstance = PlayerHolder.getInstance();
 
         this.currentPlayer = holderInstance.getPlayerHolded();
-        
+
         this.currentPlayer = (this.currentPlayer == null) ? new Player("PROVA") : this.currentPlayer;
-    }  
-    
-    /**
-     * Override this method to set your own game
-     */
-    abstract protected void setGame();
+    }
 
     /**
-    * Method to play a sound
-    * @param soundFilePath the path of the sound file in this format: "/sounds/sound.mp3"
-    * @throws IllegalArgumentException if the file is not found or the path is null
-    */
-    protected void playSound(String soundFilePath){
+     * Method to get and set an image in the ImageView
+     * 
+     * @param imageFilePath the path of the image file in this format: "image.png"
+     * @return the ImageView with the image set
+     * @throws IllegalArgumentException if the file is not found or the path is null
+     */
+    protected ImageView getImage(String imageFilePath) {
+        Image sprite;
+
+        if (imageFilePath == null) {
+            throw new IllegalArgumentException("File path is null");
+        }
+
+        // Check the path (in resources we have different directory) and load the image
+        try {
+            if (imageFilePath.contains("chip")) {
+                sprite = new Image(getClass().getResourceAsStream("/sprite/chips/" + imageFilePath));
+            } else if (imageFilePath.contains("card")) {
+                sprite = new Image(getClass().getResourceAsStream("/sprite/cards/" + imageFilePath));
+            } else if (imageFilePath.contains("die")) {
+                sprite = new Image(getClass().getResourceAsStream("/sprite/dice/" + imageFilePath));
+            } else if (imageFilePath.contains("Screen")) {
+                sprite = new Image(getClass().getResourceAsStream("/sprite/screenshot/" + imageFilePath));
+            } else {
+                throw new IllegalArgumentException("File not found: " + imageFilePath);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error during image load: " + imageFilePath);
+        }
+
+        // Set the image in the ImageView
+        ImageView spriteView = new ImageView(sprite);
+        return spriteView;
+    }
+
+    /**
+     * Displays an alert dialog with the specified type, title, and content.
+     * <p>
+     * This method creates an {@link Alert} of the given {@code AlertType}, sets its
+     * title and content,
+     * and displays it to the user. The header text is set to {@code null}.
+     * The alert waits for the user to close it before proceeding.
+     *
+     * @param type    the type of alert (e.g., {@link AlertType#INFORMATION},
+     *                {@link AlertType#ERROR})
+     * @param title   the title of the alert window
+     * @param content the message to be displayed in the alert
+     */
+    protected void showAlert(AlertType type, String title, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    /**
+     * Method to play a sound
+     * 
+     * @param soundFilePath the path of the sound file in this format:
+     *                      "/sounds/sound.mp3"
+     * @throws IllegalArgumentException if the file is not found or the path is null
+     */
+    protected void playSound(String soundFilePath) {
         URL resource = getClass().getResource(soundFilePath);
 
         if (resource == null) {
@@ -82,40 +138,5 @@ public abstract class BaseController implements Initializable {
         } catch (Exception e) {
             throw new IllegalArgumentException("Error playing sound: " + soundFilePath);
         }
-    }
-
-    /**
-    * Method to get and set an image in the ImageView
-    * @param imageFilePath the path of the image file in this format: "image.png"
-    * @return the ImageView with the image set
-    * @throws IllegalArgumentException if the file is not found or the path is null
-    */
-    protected ImageView getImage(String imageFilePath){
-        Image sprite;
-
-        if(imageFilePath == null){
-            throw new IllegalArgumentException("File path is null");
-        } 
-
-        // Check the path (in resources we have different directory) and load the image
-        try {
-            if (imageFilePath.contains("chip")) {
-                sprite = new Image(getClass().getResourceAsStream("/sprite/chips/" + imageFilePath));   
-            } else if (imageFilePath.contains("card")) {
-                sprite = new Image(getClass().getResourceAsStream("/sprite/cards/" + imageFilePath));   
-            } else if (imageFilePath.contains("die")) {
-                sprite = new Image(getClass().getResourceAsStream("/sprite/dice/" + imageFilePath));   
-            } else if (imageFilePath.contains("Screen")) {
-                sprite = new Image(getClass().getResourceAsStream("/sprite/screenshot/" + imageFilePath));
-            } else {
-                throw new IllegalArgumentException("File not found: " + imageFilePath);
-            }
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error during image load: " + imageFilePath);
-        }
-
-        // Set the image in the ImageView
-        ImageView spriteView = new ImageView(sprite);
-        return spriteView;
     }
 }
